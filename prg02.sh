273917fb50379be3b3141355c3c84cb592dccfc9
vlavanya <vlavanya@alclabs.in>
2023-04-28 17:52:02 +0530

    committed by local git - lavanya

--- prg02.sh
+++ prg02.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "you are testing pre and post increment and decrement operator "
+echo "you are testing pre and post increment and decrement operator "
 echo "enter a number -value of x: "
 read x # where x is a variable obtained its value from keyboard
-echo "value after post increment of x by executing x++ is $[x++]"
+echo "value after post increment of x by executing x++ is $[xprg02.sh++]"
 echo "value after pre increment of x by executing ++x is $[++x]"
 echo "value after post decrement of x by executing x-- is $[x--]"
 echo "value after pre decrement of x by executing --x is $[--x]"
